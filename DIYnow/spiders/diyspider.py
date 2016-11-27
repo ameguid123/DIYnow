7f0013262e6ff4ee78ec4811d330673c1a012418
ameguid123 <adham_meguid@college.harvard.edu>
2016-11-26 20:25:56 -0500

    Get Random Category html from Sitemap
    Have learned to navigate Makezine’s sitemap page to get the html of a
    random one of its diyproject categories. Will be used to choose a
    random project within the random category to display on the website.

--- DIYnow/spiders/diyspider.py
+++ DIYnow/spiders/diyspider.py
@@ -2,15 +2,21 @@
 
 # makezine sitemap http://makezine.com/sitemap/
 # instructables sitemap http://www.instructables.com/sitemap/instructables/
+
 from scrapy.spiders import Spider
 from DIYnow.items import DiynowItem
 from scrapy.http import Request
 import random
 
 class TitlesSpider(Spider):
+	# name of the spider, used to launch the spider
 	name = "titles"
+
 	# allowed_domains = "makezine.com"
-	start_urls = ["https://makezine.com/projects/"]
+
+	# a list of URLs that the crawler will start at
+	start_urls = ["https://makezine.com/sitemap/"]
+
 	# start_urls = [
 		# "https://www.instructables.com/",
 	#	"https://makezine.com/projects/"
@@ -19,15 +25,22 @@ class TitlesSpider(Spider):
 	#	yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
-		infos = response.xpath('//*[@itemprop ]/a')
-		for info in infos:
-			item = DiynowItem()
-			process_info(info, item)
-			yield item
+		# list of html elements with this xpath (the project categories)
+		categories = response.xpath('//li[contains(@class, "title")]/a')
+		# the html of our first random project cateogry
+		cat1 = "https://makezine.com/sitemap/" + str(
+			# get a list of project cateogry htmls from the sitemap
+			(categories.xpath('@href').extract())
+			# choose random project category html from list
+			[random.randrange(0, len(categories))])
+		#yield Request(cat1, self.parse)
+		#item = DiynowItem()
+		#process_info(cat1, item)
+		#yield item
 
 
 def process_info(info, item):
-	item["title"] = info.xpath('text()').extract()
-	item["html"] = info.xpath('@href').extract()
+	#item["title"] = info.xpath('text()').extract()
+	item["html"] = info#.xpath('@href').extract()
 
 
