b325a049bb8bf2e20b0afe77c5f0429cfb0fc99e
ameguid123 <adham_meguid@college.harvard.edu>
2016-11-27 16:36:05 -0500

    Get Multiple Random Projects Makezine
    Updated MakezineSpider to retrieve multiple random projects, as set by
    NUM_MAKEZINE_PROJECTS. Also cleaned up and added some comments.

--- DIYnow/spiders/diyspider.py
+++ DIYnow/spiders/diyspider.py
@@ -3,60 +3,53 @@
 # makezine sitemap http://makezine.com/sitemap/
 # instructables sitemap http://www.instructables.com/sitemap/instructables/
 #
-#
+# TODO
+# Exclude education category from makezine searches
+# Exclude maker news category from makezine searches
+# Exclude uncategorized category from makezine searches?
+# Check if site has not chnaged its format, if no proceed, if yes use backup site
+# 	(if no snippet returned, use different site)
 
 from scrapy.spiders import Spider
 from DIYnow.items import DiynowItem
 from scrapy.http import Request
 import random
 
-class TitlesSpider(Spider):
-	# name of the spider, used to launch the spider
-	name = "titles"
+NUM_MAKEZINE_PROJECTS = 3
 
-	# allowed_domains = "makezine.com"
+class MakezineSpider(Spider):
+	# name of the spider, used to launch the spider
+	name = "Makezine"
 
 	# a list of URLs that the crawler will start at
 	start_urls = ["http://makezine.com/sitemap/"]
 
-	# start_urls = [
-	# "https://www.instructables.com/",
-	#"https://makezine.com/projects/"
-	#]
-	#for url in urls:
-	#yield scrapy.Request(url=url, callback=self.parse)
-
 	def parse(self, response):
 		# list of html elements with this xpath (the project categories)
 		categories = response.xpath('//li[contains(@class, "title")]/a')
-		if categories.extract_first() is not None:
-			# join our current url with the next category
-			cat1 = response.urljoin(
-				# get a list of project cateogry htmls from the sitemap
-				(categories.xpath('@href').extract())
-				# choose random project category html from list
-				[random.randrange(0, len(categories))])
 
-			yield Request(cat1, callback = self.parse_makezine_projects)
-			#
-			# fixing redirect
-			# http://stackoverflow.com/questions/22795416/how-to-handle-302-redirect-in-scrapy
-			#yield Request(cat1, meta = {
-            #      'dont_redirect': True,
-            #      'handle_httpstatus_list': [301]
-            #  },callback = self.parse_makezine_projects)
+		# ensure this list is not empty (site format is same)
+		if categories.extract_first() is not None:
+
+			for i in range(NUM_MAKEZINE_PROJECTS):
+				# join our current url with the next category
+				category = response.urljoin(
+					# get a list of project cateogry htmls from the sitemap
+					(categories.xpath('@href').extract())
+					# choose random project category html from list
+					[random.randrange(0, len(categories))])
+
+				yield Request(category, callback = self.parse_makezine_projects)
 
 	def parse_makezine_projects(self, response):
-		item = DiynowItem()
 
 		# list of html elements with xpath that leads to project link
 		projects = response.xpath('//ul[contains(@class, "sitemap_links")]/li/a')
 
+		# declare instance of a DiynowItem, fill in its fields, then yield
 		item = DiynowItem()
 		process_info(projects, item)
 		yield item
-
-
 
 def process_info(projects, item):
 	# chose a random number between 0 and the number of projects in projects
