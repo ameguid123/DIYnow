fe56a667c669e16cde6813debabad8f6c54eb221
ameguid123 <adham_meguid@college.harvard.edu>
2016-12-01 21:11:05 -0500

    Running on Flask and Installed Dependencies
    Installed website dependencies (SQL, Flask, etc.) and can now serve
    basic front end on Flask. Also created a virtual environment for
    working in.

---
+++ application.py
@@ -0,0 +1,66 @@
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
+from datetime import datetime
+from time import gmtime, strftime
+import sqlite3
+import hashlib
+
+from helpers import *
+
+
+# -----------------------------database creation--------------------------------
+# https://docs.python.org/3.6/library/sqlite3.html#sqlite3.Connection
+# create a Connection object that represents the database.
+conn = sqlite3.connect("DIYnow.db")
+
+# creating a cursor object for executing SQL commands
+c = conn.cursor()
+
+
+
+# ---------------------------------flask setup----------------------------------
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+# CREDIT PSET7
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+# export FLASK_APP=application.py
+# ---------------------------------END SETUP------------------------------------
+
+@app.route("/")
+#@login_required
+def index():
+	return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	return render_template("index.html");
+
+# # Create table
+# c.execute('''CREATE TABLE stocks
+#              (date text, trans text, symbol text, qty real, price real)''')
+
+# # Insert a row of data
+# c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# # Save (commit) the changes
+# conn.commit()
+
+# # We can also close the connection if we are done with it.
+# # Just be sure any changes have been committed or they will be lost.
+# conn.close()
